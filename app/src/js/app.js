c96ae49e8c713976640d18329774e51094b225be
mkhlil1288 <mkhlil1288@gmail.com>
2016-09-07 01:08:13 +0200

    responsive nav

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -9,64 +9,70 @@ window.jQuery = $;
 console.log($.fn.jquery);
 
 import ScrollReveal from './modules/scrollreveal';
+    // https://github.com/liabru/jquery-match-height
 import matchHeight from './modules/jquery.matchHeight';
+    // https://github.com/WeCodePixels/theia-sticky-sidebar
 import theiaStickySidebar from'./modules/theia-sticky-sidebar';
+    // https://github.com/garand/sticky
 import sticky from'./modules/jquery.sticky';
 import sidr from'./modules/jquery.sidr';
+// import velocity from'./modules/velocity';
+import animatedModal from'./modules/animatedModal';
 
 $(document).ready(function () {
 
-
+    // loading bar
     Pace.on("done", function(){
         $("#wrapper").fadeIn(2000);
     });
 
-
-
-	// Loader
-	$(window).on('load', function(){
-		setTimeout(function () {
-		    $('#loader').css('display', 'none');
-		}, 300);
-	});
-
-	
-
-
-	// ScrollReveal
-	// window.sr = ScrollReveal().reveal('.header');
-
-
-
-    // Login DropDown Form 
-    $('#session').click(function () {
-        if ($('#signin-dropdown').is(":visible")) {
-            $('#signin-dropdown').hide()
-            $('#session').removeClass('active'); // When the dropdown is not visible removes the class "active"
-        } else {
-            $('#signin-dropdown').show()
-            $('#session').addClass('active'); // When the dropdown is visible add class "active"
-        }
-        return false;
+    // login form in top bar
+    $("#login-link").animatedModal({
+      beforeOpen: function() {
+        setTimeout( function(){
+          $(".login-form").show().velocity("slideDown", { duration: 200 });
+        } , 600 );
+      },
+      afterClose: function() {
+        $(".login-form").hide();
+      }
     });
 
+    
+  $("header").height($(window).height() - 40);
+
+  // window.sr = ScrollReveal().reveal('.header');
 
 
-    // Release when click outside
-    $('#signin-dropdown').click(function(e) {
-        e.stopPropagation();
+    // $('.item').matchHeight(options);
+
+
+    // $("#sticker").sticky({topSpacing:0});
+    // $("#sticker").unstick();
+
+
+    // $('.content, .sidebar').theiaStickySidebar({
+    //   // Settings
+    //   additionalMarginTop: 30
+    // });
+
+    $('.mobile_menu').on('click', function(event){
+      if ( $('nav').is(":visible") ) {
+        $('nav').velocity("slideUp", { duration: 200 });
+      }else {
+        $('nav').velocity("slideDown", { duration: 200 });
+      }
     });
-
-    $(document).click(function() {
-        $('#signin-dropdown').hide();
-        $('#session').removeClass('active');
-    });
-
-
 
     // Side Menu
     $('#toggle-menu').sidr({
         side: 'right'
+    });
+
+
+
+    $('.go-top').on('click', function (event) {
+        $('html, body').velocity('scroll',{duration: 1000, offset:0});
     });
 
 });
